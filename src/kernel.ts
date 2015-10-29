4da1fdd7c2979895a7b299736ef9f35bcdac94f5
Steven Silvester <steven.silvester@ieee.org>
2015-10-29 17:26:11 -0500

    Implement kernel and session

--- src/kernel.ts
+++ src/kernel.ts
@@ -9,14 +9,85 @@ import {
 
 import {
   IDisposable, DisposableDelegate
-} from 'phoshor-disposable';
+} from 'phosphor-disposable';
 
 import {
   IExtension
 } from 'phosphor-plugins';
 
 
+/**
+ * Get the list of registered kernel providers.
+ */
+export
+function listKernelProviders(): string[] {
+  return Object.keys(providers);
+}
 
+
+/**
+ * Get the kernel specs from a given provider.
+ */
+export 
+function getKernelSpecs(providerName: string, baseUrl: string, ajaxOptions?: IAjaxOptions): Promise<IKernelSpecIds> {
+  let provider = providers[providerName];
+  if (provider) {
+    return provider.getKernelSpecs(baseUrl, ajaxOptions);
+  }
+}
+
+
+/**
+ * List the running kernels on a given provider.
+ */
+export 
+function listRunningKernels(providerName: string, baseUrl: string, ajaxOptions?: IAjaxOptions): Promise<IKernelId[]> {
+  let provider = providers[providerName];
+  if (provider) {
+    return provider.listRunningKernels(baseUrl, ajaxOptions);
+  }
+}
+
+
+/**
+ * Start a new kernel on the given provider.
+ */
+export 
+function startNewKernel(providerName: string, options: IKernelOptions, ajaxOptions?: IAjaxOptions): Promise<IKernel> {
+  let provider = providers[providerName];
+  if (provider) {
+    return provider.startNewKernel(options, ajaxOptions);
+  }
+}
+
+
+/**
+ * Connect to an existing kernel on the given provider.
+ */
+export 
+function connectToKernel(providerName: string, id: string, options?: IKernelOptions, ajaxOptions?: IAjaxOptions): Promise<IKernel> {
+  let provider = providers[providerName];
+  if (provider) {
+    return provider.connectToKernel(id, options, ajaxOptions);
+  }
+}
+
+
+/**
+ * Create a well-formed kernel message using the given provider.
+ */
+export 
+function createKernelMessage(providerName: string, options: IKernelMessageOptions, content: any = {}, metadata: any = {}, buffers: (ArrayBuffer | ArrayBufferView)[] = []): IKernelMessage {
+  let provider = providers[providerName];
+  if (provider) {
+    return provider.createKernelMessage(options, content, metadata, buffers);
+  }
+}
+
+
+/**
+ * Interface for a Kernel Provider extension object.
+ */
 export 
 interface IKernelProvider {
   name: string;
@@ -28,28 +99,19 @@ interface IKernelProvider {
 }
 
 
-export
-function listKernelProviders(): string[] {
-  return (Array as any).from(providers.keys());
-}
-
-
+/**
+ * Kernel provider extension point handler.
+ */
 export
 function receiveKernelProvider(ext: IExtension<IKernelProvider>): IDisposable {
-  providers.set(ext.name, ext.object);
+  providers[ext.object.name] = ext.object;
   return new DisposableDelegate(() => {
-    providers.delete(ext.name);
+    if (ext.object.name in providers) {
+      delete providers[ext.object.name];
+    }
   });
 }
 
 
-export 
-function getKernelSpecs(providerName: string, baseUrl: string, ajaxOptions?: IAjaxOptions): Promise<IKernelSpecIds> {
-  let provider = providers.get(providerName);
-  if (provider) {
-    return provider.getKernelSpecs(baseUrl, ajaxOptions);
-  }
-}
-
-
-var providers = new Map<string, IKernelProvider>();
+// Map of available Kernel providers.
+var providers: { [key: string]: IKernelProvider } = { };
--- src/session.ts
+++ src/session.ts
@@ -3,18 +3,89 @@
 'use strict';
 
 import { 
-  listRunningSessions, startNewSession, connectToSession 
+  IAjaxOptions, INotebookSession, ISessionId, ISessionOptions, 
+  connectToSession, listRunningSessions, startNewSession
 } from 'jupyter-js-services';
+
+import {
+  IDisposable, DisposableDelegate
+} from 'phosphor-disposable';
+
+import {
+  IExtension
+} from 'phosphor-plugins';
 
 
 /**
- * Return a namespace with the exported function.
+ * Get the list of registered session providers.
  */
 export
-function loader(): any {
-  return {
-    'listRunningSessions': listRunningSessions,
-    'startNewSession': startNewSession,
-    'connectToSession': connectToSession
+function listSessionProviders(): string[] {
+  return Object.keys(providers);
+}
+
+
+/**
+ * List the running sessions on a given provider.
+ */
+export 
+function listRunningSessions(providerName: string, baseUrl: string, ajaxOptions?: IAjaxOptions): Promise<ISessionId[]> {
+  let provider = providers[providerName];
+  if (provider) {
+    return provider.listRunningSessions(baseUrl, ajaxOptions);
   }
 }
+
+
+/**
+ * Start a new session on the given provider.
+ */
+export 
+function startNewSession(providerName: string, options: ISessionOptions, ajaxOptions?: IAjaxOptions): Promise<INotebookSession> {
+  let provider = providers[providerName];
+  if (provider) {
+    return provider.startNewSession(options, ajaxOptions);
+  }
+}
+
+
+/**
+ * Connect to an existing session on the given provider.
+ */
+export 
+function connectToSession(providerName: string, id: string, options?: ISessionOptions, ajaxOptions?: IAjaxOptions): Promise<INotebookSession> {
+  let provider = providers[providerName];
+  if (provider) {
+    return provider.connectToSession(id, options, ajaxOptions);
+  }
+}
+
+
+/**
+ * Interface for a Session Provider extension object.
+ */
+export 
+interface ISessionProvider {
+  name: string;
+  listRunningSessions: (baseUrl: string, ajaxOptions?: IAjaxOptions) => Promise<ISessionId[]>;
+  startNewSession: (options: ISessionOptions, ajaxOptions?: IAjaxOptions) =>Promise<INotebookSession>;
+  connectToSession: (id: string, options?: ISessionOptions, ajaxOptions?: IAjaxOptions) => Promise<INotebookSession>;
+}
+
+
+/**
+ * Session provider extension point handler.
+ */
+export
+function receiveSessionProvider(ext: IExtension<ISessionProvider>): IDisposable {
+  providers[ext.object.name] = ext.object;
+  return new DisposableDelegate(() => {
+    if (ext.object.name in providers) {
+      delete providers[ext.object.name];
+    }
+  });
+}
+
+
+// Map of available Session providers.
+var providers: { [key: string]: ISessionProvider } = { };
